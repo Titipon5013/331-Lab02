5f4931679c51342e169ca58e65248ada8907ebc9
Titipon Tawong <titipon_tawong@cmu.ac.th>
2025-07-03 19:30:36 +0700

    8.7

---
+++ 331-Lab02/src/components/CategoryOrganizer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { Event } from '@/type'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="category-organizer">
+    <p class="text">Category: {{ event.category }}</p>
+    <p class="text">Organizer: {{ event.organizer }}</p>
+  </div>
+</template>
+
+<style scoped>
+.category-organizer {
+  padding: 10px;
+  margin: 5px;
+}
+
+.text {
+  font-size: 16px;
+  text-align: right;
+  margin: 5px 0;
+}
+</style>
--- 331-Lab02/src/components/EventCard.vue
+++ 331-Lab02/src/components/EventCard.vue
@@ -4,23 +4,28 @@ import { ref } from 'vue'
 //   msg: string
 // }>()
 
-const event = ref({
-  id: 5928101,
-  category: 'animal walfare',
-  titile: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date:'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee'
-})
+import type { Event } from '@/type'
+defineProps<{
+    event: Event
+}>()
+
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal walfare',
+//   titile: 'Cat Adoption Day',
+//   description: 'Find your new feline friend at this event.',
+//   location: 'Meow Town',
+//   date:'January 28, 2022',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Kat Laydee'
+// })
 </script>
 
 <template>
 <div class="event-class">
     <div class="event-card">
-        <h2>{{ event.titile }}</h2>
+        <h2>{{ event.title }}</h2>
         <span>@{{ event.time }} on {{ event.date }} </span>
     </div>
 </div>        
