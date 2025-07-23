599ce162c1ad448cdb44f337c9ff150a12446690
Titipon Tawong <titipon_tawong@cmu.ac.th>
2025-07-24 03:09:24 +0700

    Lab03-5.2

--- 331-Lab02/src/router/index.ts
+++ 331-Lab02/src/router/index.ts
@@ -7,6 +7,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import StudentListView from '@/views/StudentListView.vue';
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -58,6 +59,11 @@ const router = createRouter({
       path: '/:catchAll(.*)',
       name: 'not-found',
       component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     },
     {
       path: '/students',
